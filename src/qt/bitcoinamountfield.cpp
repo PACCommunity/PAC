6b6185c11096f63d05c173380fbe4e821e2beb67
Kevin <kevin_defimex@hotmail.com>
2019-01-24 13:55:56 -0600

    Layout alignment

--- src/qt/bitcoinamountfield.cpp
+++ src/qt/bitcoinamountfield.cpp
@@ -197,7 +197,7 @@ BitcoinAmountField::BitcoinAmountField(QWidget *parent) :
     amount = new AmountSpinBox(this);
     amount->setLocale(QLocale::c());
     amount->installEventFilter(this);
-    amount->setMaximumWidth(170);
+    //amount->setMaximumWidth(170);
 
     QHBoxLayout *layout = new QHBoxLayout(this);
     layout->addWidget(amount);
