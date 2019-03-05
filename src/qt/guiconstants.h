521c9cc5dc75fb40da1746e20152f8b5960f1dcb
Carlos Iván Ibarra Pacheco <bytepacheco@gmail.com>
2019-03-05 11:40:49 -0600

    Qslider customized and sendcoinsdialog

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -20,7 +20,7 @@ static const int STATUSBAR_ICONSIZE = 16;
 static const bool DEFAULT_SPLASHSCREEN = true;
 
 /* Invalid field background style */
-#define STYLE_INVALID "background:#FF8080"
+#define STYLE_INVALID "background:#ff4d4d"
 
 /* Transaction list -- unconfirmed transaction */
 #define COLOR_UNCONFIRMED QColor(128, 128, 128)
